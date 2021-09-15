54bd8a5e30ea78683dee48f648b97a3c7b3b4238
tjconti12 <78386402+tjconti12@users.noreply.github.com>
2021-09-15 17:11:52 -0400

    organized server.js

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const creds = require('./config');
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+
+
+
+// Nodemailer transport object
 const transport = {
     host: 'smtp.gmail.com',
     port: 587,
@@ -59,7 +70,7 @@ router.post('/send', (req, res, next) => {
             res.json({
                 status: 'success'
             })
-
+            // Sending the autoreply
             transporter.sendMail({
                 from: creds.USER,
                 to: email,
@@ -76,8 +87,6 @@ router.post('/send', (req, res, next) => {
     })
 })
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/', router);
-app.listen(3002);
+
+
+app.listen(PORT);
