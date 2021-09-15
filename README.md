864c26cc1efe37afe03c546640b7b241c380497c
tjconti12 <78386402+tjconti12@users.noreply.github.com>
2021-09-15 16:21:22 -0400

    server accepts email requests, forwards to developers email, and auto replies to the user

---
+++ README.md
@@ -0,0 +1,20 @@
+# Email Server
+Email server for portfolio page https://www.tylerjconti.com
+
+Currently in progress. In development (updated as of 9/15)
+
+## Build Status
+In build phase
+
+## Tech/framework used
+1. Javascript
+2. Express
+
+## Features
+Accepts email requests from portfolio site, and forwards them to my email. Also will send user a auto reply email confirmation
+
+
+## License
+&copy; tjconti12
+
+
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const creds = require('./config');
+
+const transport = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: creds.USER,
+        pass: creds.PASS
+    }
+}
+
+const transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('Server is ready to take messages')
+    }
+});
+
+router.post('/send', (req, res, next) => {
+    let name = req.body.name;
+    let email = req.body.email
+    let message = req.body.message
+    let content = `name: ${name} \n email: ${email} \n message: ${message}`
+
+    let mail = {
+        from: name,
+        to: creds.USER,
+        subject: 'New Message from Contact Form',
+        text: content
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+        if(err) {
+            res.json({
+                status: 'fail'
+            })
+        } else {
+            res.json({
+                status: 'success'
+            })
+
+            transporter.sendMail({
+                from: creds.USER,
+                to: email,
+                subject: "Submission was successful",
+                text: `Thank you for contacting me ${name}! I will get back to you as soon as possible. \n\nTyler`
+            }, function(error, info) {
+                if(error) {
+                    console.log(error)
+                } else {
+                    console.log('Message send: ' + info.response);
+                }
+            });
+        }
+    })
+})
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+app.listen(3002);
